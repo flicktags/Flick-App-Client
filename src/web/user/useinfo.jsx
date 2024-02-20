d5539f81ca4e2fb34e6cc6849117c1dfc778e408
flicktags <159721553+flicktags@users.noreply.github.com>
2024-02-20 17:40:54 +0500

    Update useinfo.jsx

--- src/web/user/useinfo.jsx
+++ src/web/user/useinfo.jsx
@@ -2,90 +2,122 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import userimg from '../assets/user1.png';
 import SocialMediaContact from './socialmediacontacts';
-import {fetchData} from '../../services/issharebycategorey';
-import {saveDataTodefault} from '../../services/issharebycategorey';
+import { fetchData } from '../../services/issharebycategorey';
+import { saveDataTodefault } from '../../services/issharebycategorey';
 import { GridLoader } from 'react-spinners';
 const UserInfo = () => {
   const [userData, setUserData] = useState(null);
   const [fetchedData, setFetchedData] = useState(null);
   const [loading, setLoading] = useState(false);
-  const tokens =userData?.deviceToken ;
+  const [cancel, setCancel] = useState(false);
+  const [value, setValue] = useState(true);
+  const tokens = userData?.deviceToken;
   localStorage.setItem('tokens', JSON.stringify(tokens));
-const userid=window.location.pathname.slice(1)
-useEffect(() => {
-  if (!userid) {
-    alert('User ID is empty');
-    return;
-  }
+  const userid = window.location.pathname.slice(1)
+  useEffect(() => {
+    if (!userid) {
+      alert('User ID is empty');
+      return;
+    }
 
-  const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-  const fetchDataWithDelay = async () => {
-    await delay(1000); 
-    fetchUserData(); 
-  };
-  saveDataTodefault(userid);
-  fetchDataWithDelay();
-}, []);
+    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    const fetchDataWithDelay = async () => {
+      await delay(1000);
+      fetchUserData();
+    };
+    saveDataTodefault({ userid, value });
+    fetchDataWithDelay();
+  }, []);
 
 
   const fetchUserData = async () => {
-    
+
     try {
       const response = await fetch(`https://flickapp.vercel.app/user/${window.location.pathname.slice(1)}`);
       const data = await response.json();
-     
+
       setUserData(data.data);
-      if (data.data.isSHareByCatgOn==true) {
+      if (data.data.isSHareByCatgOn == true) {
         // If user is shareable by category, start fetching category data
-        
+
         fetchCategoryData();
       }
     } catch (error) {
       console.error('Error fetching user details:', error);
-      
+
     }
   };
 
-
   const fetchCategoryData = async () => {
-    const timer = setTimeout(() => {
+    let newData = null;
+    let timer;
+    const timers = setTimeout(() => {
       console.log('Fetching user details')
       sendNotificationToUser(userData?.deviceToken);
-      
+
     }, 1000);
-   
+    const handleTimeout = () => {
+
+      if (newData?.selectedCatgBtnOptionValue === 'default') {
+        const value = false;
+        saveDataTodefault({ userid, value });
+        setCancel(true);
+      }
+
+    };
+
+    const startTimer = () => {
+      timer = setTimeout(handleTimeout, 10000);
+    };
+
+    const stopTimer = () => {
+      clearTimeout(timer);
+    };
+
     setLoading(true);
+
     try {
-      let newData = null;
-      // Fetch data until selectedCatgBtnOptionValue is updated
-      while (!newData ||newData?.selectedCatgBtnOptionValue=='default') {
+      startTimer();
+      while (!newData || newData?.selectedCatgBtnOptionValue === 'default') {
         newData = await fetchData(userid);
         setFetchedData(newData);
-        await new Promise(resolve => setTimeout(resolve, 2000)); // Wait for 5 seconds before next fetch
+        await new Promise(resolve => setTimeout(resolve, 6000));
       }
     } catch (error) {
       console.error('Error fetching category data:', error);
+    } finally {
+      stopTimer();
+      setLoading(false);
     }
   };
-  if ( fetchedData?.selectedCatgBtnOptionValue =='default') {
+  if (cancel == true) {
+    return (< div className='auto_cancel'>
+      <h1>Apologies</h1>
+      <p className='cancel_data'>We regret to inform you that the user is currently unable to respond.</p>
+      <p className='cancel_data'>Please try again later.</p>
+    </div>);
+  }
+
+  if (fetchedData?.selectedCatgBtnOptionValue == 'default') {
     return (
       <div className='spinner'>
-        <GridLoader color={'#aeb5cf'} loading={loading} size={30}/>
+        <GridLoader color={'#aeb5cf'} loading={loading} size={30} />
         <h1>Please Wait For User </h1>
         <h1>Response</h1>
       </div>
     );
   }
-  if ( fetchedData?.selectedCatgBtnOptionValue =='canceled') {
+  if (fetchedData?.selectedCatgBtnOptionValue == 'canceled') {
     return (
       <div class="canceltext-container">
-  <div class="canceltext">
-    <h1>Your request has been canceled by the user. Please try again</h1>
-  </div>
-</div>
+        <div class="canceltext">
+          <h1>Your request has been canceled by the user. Please try again</h1>
+        </div>
+      </div>
     );
   }
- if(fetchedData?.selectedCatgBtnOptionValue === 'Business' || fetchedData?.selectedCatgBtnOptionValue === 'business') {
+  if (fetchedData?.selectedCatgBtnOptionValue === 'Business' || fetchedData?.selectedCatgBtnOptionValue === 'business') {
+    console.log('============================================================')
     return (
       <div>
         <div className='overlay'>
@@ -94,7 +126,7 @@ useEffect(() => {
               <img src={userData?.userImage || userimg} alt='' className='userimg' title='Click to view full image' />
             </div>
             <div className='usrdta'>
-              
+
               <h1>{userData?.name}</h1>
               <p className='profession'>{userData?.profession}</p>
               <p>{userData?.organization}</p>
@@ -105,7 +137,7 @@ useEffect(() => {
           <div>
             <div>
               {userData?.socialMedia
-                .filter((socialMedia) => socialMedia.category=='Business'&& socialMedia.isActive==true)
+                .filter((socialMedia) => socialMedia.category == 'Business' && socialMedia.isActive == true)
                 .map((socialMedia) => (
                   <SocialMediaContact
                     key={socialMedia._id}
@@ -123,8 +155,8 @@ useEffect(() => {
       </div>
     );
   }
-  if (fetchedData?.selectedCatgBtnOptionValue=='public') {
-    
+  if (fetchedData?.selectedCatgBtnOptionValue == 'public') {
+
     return (
       <div>
         <div className='overlay'>
@@ -133,7 +165,7 @@ useEffect(() => {
               <img src={userData?.userImage || userimg} alt='' className='userimg' title='Click to view full image' />
             </div>
             <div className='usrdta'>
-              
+
               <h1>{userData?.name}</h1>
               <p className='profession'>{userData?.profession}</p>
               <p>{userData?.organization}</p>
@@ -144,7 +176,7 @@ useEffect(() => {
           <div>
             <div>
               {userData?.socialMedia
-                .filter((socialMedia) => socialMedia.category==='Public'&& socialMedia.isActive==true)
+                .filter((socialMedia) => socialMedia.category === 'Public' && socialMedia.isActive == true)
                 .map((socialMedia) => (
                   <SocialMediaContact
                     key={socialMedia._id}
@@ -162,7 +194,7 @@ useEffect(() => {
       </div>
     );
   }
-  if (fetchedData?.selectedCatgBtnOptionValue=='all') {
+  if (fetchedData?.selectedCatgBtnOptionValue == 'all') {
     return (
       <div>
         <div className='overlay'>
@@ -171,7 +203,7 @@ useEffect(() => {
               <img src={userData?.userImage || userimg} alt='' className='userimg' title='Click to view full image' />
             </div>
             <div className='usrdta'>
-              
+
               <h1>{userData?.name}</h1>
               <p className='profession'>{userData?.profession}</p>
               <p>{userData?.organization}</p>
@@ -200,7 +232,7 @@ useEffect(() => {
       </div>
     );
   }
-  
+
   if (userData && userData.isActive === true) {
     setTimeout(() => {
       alert('This account is private and cannot be viewed');
@@ -215,75 +247,75 @@ useEffect(() => {
   }
   async function sendNotificationToUser() {
     try {
-        // Retrieve tokens from local storage
-        const storedTokens = localStorage.getItem('tokens');
-        const tokens = storedTokens ? JSON.parse(storedTokens) : [];
-        
+      // Retrieve tokens from local storage
+      const storedTokens = localStorage.getItem('tokens');
+      const tokens = storedTokens ? JSON.parse(storedTokens) : [];
 
-        // Iterate over each device token
-        for (let i = 0; i < tokens.length; i++) {
-            const token = tokens[i];
-            // Make an HTTP request to FCM API for each token
-            const response = await axios.post('https://fcm.googleapis.com/fcm/send', {
-                to: token, 
-                notification: {
-                    title: "Please select Social Media Handles",
-                    body: "Please select Social Media Handles",
-                },
-            }, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer AAAA3T3WtjY:APA91bFEkTNtpWPbxSe7x-sgQjusAirIBm0bqDqrAYMmJOIybEDtq0wVmVUDxOCKkyMk96G58AQ3cno3j_iUUzNe2GKuvKB8OlzA9tT0nFzkQgiyOHFqSSBb79wy0dYcjGpBPEvCTaen`, 
-                },
-            });
 
-            console.log('Notification sent successfully to token:', token);
-            console.log('Response:', response.data);
-        }
+      // Iterate over each device token
+      for (let i = 0; i < tokens.length; i++) {
+        const token = tokens[i];
+        // Make an HTTP request to FCM API for each token
+        const response = await axios.post('https://fcm.googleapis.com/fcm/send', {
+          to: token,
+          notification: {
+            title: "Please select Social Media Handles",
+            body: "Please select Social Media Handles",
+          },
+        }, {
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer AAAA3T3WtjY:APA91bFEkTNtpWPbxSe7x-sgQjusAirIBm0bqDqrAYMmJOIybEDtq0wVmVUDxOCKkyMk96G58AQ3cno3j_iUUzNe2GKuvKB8OlzA9tT0nFzkQgiyOHFqSSBb79wy0dYcjGpBPEvCTaen`,
+          },
+        });
+
+        console.log('Notification sent successfully to token:', token);
+        console.log('Response:', response.data);
+      }
     } catch (error) {
-        console.error('Error sending notification:', error);
+      console.error('Error sending notification:', error);
     }
-}
+  }
 
 
-    if (userData?.isSHareByCatgOn==false) {
-      return (
-        <div>
-          <div className='overlay'>
-            <div className='modal'>
-              <div className='userimgcontainer'>
-                <img src={userData?.userImage || userimg} alt='' className='userimg' title='Click to view full image' />
-              </div>
-              <div className='usrdta'>
-                
-                <h1>{userData?.name}</h1>
-                <p className='profession'>{userData?.profession}</p>
-                <p>{userData?.organization}</p>
-              </div>
+  if (userData?.isSHareByCatgOn == false) {
+    return (
+      <div>
+        <div className='overlay'>
+          <div className='modal'>
+            <div className='userimgcontainer'>
+              <img src={userData?.userImage || userimg} alt='' className='userimg' title='Click to view full image' />
             </div>
-          </div>
-          <div>
-            <div>
-              <div>
-                {userData?.socialMedia
-                  .filter((socialMedia) => socialMedia.isActive)
-                  .map((socialMedia) => (
-                    <SocialMediaContact
-                      key={socialMedia._id}
-                      socialMediaType={socialMedia.socialMediaType}
-                      socialMediaName={socialMedia.socialMediaName}
-                      socialMedialink={socialMedia.socialMediaLink}
-                      userDirectMode={userData.directMode}
-                      socialMediaDirectMode={socialMedia.socialMediaDirectMode}
-                      cat={socialMedia.category}
-                    />
-                  ))}
-              </div>
+            <div className='usrdta'>
+
+              <h1>{userData?.name}</h1>
+              <p className='profession'>{userData?.profession}</p>
+              <p>{userData?.organization}</p>
             </div>
           </div>
         </div>
-      );
-    }
+        <div>
+          <div>
+            <div>
+              {userData?.socialMedia
+                .filter((socialMedia) => socialMedia.isActive)
+                .map((socialMedia) => (
+                  <SocialMediaContact
+                    key={socialMedia._id}
+                    socialMediaType={socialMedia.socialMediaType}
+                    socialMediaName={socialMedia.socialMediaName}
+                    socialMedialink={socialMedia.socialMediaLink}
+                    userDirectMode={userData.directMode}
+                    socialMediaDirectMode={socialMedia.socialMediaDirectMode}
+                    cat={socialMedia.category}
+                  />
+                ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default UserInfo;
