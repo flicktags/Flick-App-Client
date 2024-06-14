e3c62cff450b18951720bebf05485748384cc48f
MFahim786 <mfahimchq@.com>
2024-06-14 19:01:24 +0500

    git

--- src/web/user/save-contact.jsx
+++ src/web/user/save-contact.jsx
@@ -2,39 +2,9 @@ import React from 'react';
 import '../styles/save-contact.css';
 
 export default function SaveContact() {
+
   const handleSaveContact = () => {
-    // Define the vCard data
-    const vCardData = `
-BEGIN:VCARD
-VERSION:3.0
-N:Isa;Shabir Ahmed;;;
-FN:Shabir Ahmed Isa
-ORG:Flick Technologies
-TITLE:Founder & CEO
-TEL;TYPE=CELL:36499889
-EMAIL:shabirhassani1@gmail.com
-URL:http://www.dummy.com
-END:VCARD
-    `;
-
-    // Create a blob from the vCard data
-    const blob = new Blob([vCardData], { type: 'text/vcard' });
-    const url = window.URL.createObjectURL(blob);
-
-    // Create a link element
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'contact.vcf';
-
-    // Append the link to the body
-    document.body.appendChild(a);
-
-    // Programmatically click the link to trigger the download
-    a.click();
-
-    // Clean up by removing the link
-    document.body.removeChild(a);
-    window.URL.revokeObjectURL(url);
+    window.open('/save-contact-form?name=Shabir+Ahmed+Isa&email=shabirhassani1@gmail.com&phone=36499889&organization=Flick+Technologies&profession=Founder+%26+CEO&website=flickURL%2BUserID'); // URL encoded dummy data
   };
 
   return (
